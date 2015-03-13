d64c69bda03b5a80a30cb40d1e9067fa9dd06dbe
Juan Cabrera <juan.acc@gmail.com>
2015-03-13 17:28:46 -0400

    :lipstick:

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-/*jshint node:true */
-
 'use strict';
 
-var map   = require('map-stream'),
-    gutil   = require('gulp-util'),
-    exec    = require('child_process').exec,
-    fs      = require('fs'),
+var map = require('map-stream'),
+    gutil = require('gulp-util'),
+    exec = require('child_process').exec,
+    fs = require('fs'),
     build_commandline = require('./lib/traceur_command_builder');
 
 module.exports = function(opt) {
+  opt = opt || {};
+  var debug = opt.debug || false;
+  var clear = opt.clear;
+  delete opt.debug;
+  delete opt.clear;
 
-    // Assign default options if one is not supplied
-    opt = opt || {};
-    var debug =   opt.debug   || false;
-    var clear = opt.clear;
-    delete opt.debug;
-    delete opt.clear;
+  return map( function(file, cb) {
+    var module_opts = {
+      clear: clear,
+      file: file,
+      traceurcmd: __dirname + "/node_modules/traceur/traceur"
+    };
 
-    return map( function(file, cb) {
-        var module_opts = {
-            clear: clear,
-            file: file,
-            traceurcmd: __dirname + "/node_modules/traceur/traceur"
-        };
+    var cmd = build_commandline(opt, module_opts);
 
-        var cmd = build_commandline(opt, module_opts);
+    if (debug)  gutil.log(gutil.colors.yellow('\n *** Debug : Command - ' + cmd  + ' ***\n'));
 
-        // print out debug details
+    exec(cmd, function(error, stdout, stderr) {
+      if (error) {
         if (debug) {
-            gutil.log(gutil.colors.yellow('\n *** Debug : Command - ' + cmd  + ' ***\n'));
+          gutil.log(error);
         }
-
-        exec(cmd, function(error, stdout, stderr) {
-            // call user callback if ano error occurs
-            if (error) {
-                if (debug) {
-                    gutil.log(error);
-                }
-                cb(error, file);
-            }
-            else {
-                cb(null, file);
-            }
-        });
+        cb(error, file);
+      } else {
+        cb(null, file);
+      }
     });
+  });
 };
