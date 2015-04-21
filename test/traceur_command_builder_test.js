e13d2cbce87d91de3821a29cd2e7a46256d27b2b
Michael Vashchinsky <mvashchinsky@outbrain.com>
2015-04-21 12:38:34 +0300

    Remove pointless test. Add require for 'expect' so the manually ran mocha does not fail.

--- test/traceur_command_builder_test.js
+++ test/traceur_command_builder_test.js
@@ -1,20 +1,9 @@
-var build_commandline = require('../lib/traceur_command_builder');
+var expect = require('chai').expect,
+	build_commandline = require('../lib/traceur_command_builder');
 
 var fake = {path: 'a.js'};
 
 describe("CommandBuilder", function() {
-	describe("command line command", function() {
-		it("check default command line command", function () {
-			var command = build_commandline({}, {file: fake});
-			expect(command).to.have.string('/usr/local/bin/traceur');
-		});
-		it("check command line command", function () {
-			var command = build_commandline({}, {file: fake, traceurcmd: 'traceur'});
-			expect(command).not.to.have.string('/usr/local/bin/traceur');
-			expect(command).to.have.string('traceur');
-		});
-	});
-
 	describe("modules option", function() {
 		it("default module option", function(){
 			var command = build_commandline({},{file: fake, traceurcmd: 'traceur'});
